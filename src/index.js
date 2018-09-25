217d313f1be20ba09475c3251a8fbf4779f3f7af
Jolene Langlinais <jolenelanglinais@gmail.com>
2018-09-25 16:42:02 -0400

    GitHub WORK

--- src/index.js
+++ src/index.js
@@ -12,10 +12,13 @@ const map = new mapboxgl.Map({
 // const markerDomEl
 // new mapboxgl.Marker('marker').setLngLat([-74.009151, 40.705086]).addTo(map);
 
-
+// ADDING THIS TO REPO
 const markerDomEl = document.createElement("div"); // Create a new, detached DIV
 markerDomEl.style.width = "32px";
 markerDomEl.style.height = "39px";
 markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
 
 new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
+
+const map = new marker({
+});
