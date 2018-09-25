1fdf939bd07483952b3f7409a49814c838d83f43
Danielle Bond <dbond603@gmail.com>
2018-09-25 16:54:18 -0400

    second commit

--- src/marker.js
+++ src/marker.js
@@ -1,12 +1,23 @@
+const mapbox = require("mapbox-gl");
 
+const iconURLs = {
+    hotels: "http://i.imgur.com/D9574Cu.png",
+    restaurants: "http://i.imgur.com/cqR6pUI.png",
+    activities: "http://i.imgur.com/WbMOfMl.png"
+};
 
-function markerMaker () {
+const buildMarker = (type, coords) => {
+    const markerDomEl = document.createElement("div");
 
+    if (type === 'hotel') {
+        markerDomEl.style.backgroundImage = iconURLs.hotels;
+    }
+    if (type === 'restaurant') {
+        markerDomEl.style.backgroundImage = iconURLs.restaurants;
+    }
+    if (type === 'activity') {
+        markerDomEl.style.backgroundImage = iconURLs.activity;
+    }
+};
 
-}
-
-
-
-
-
-module.exports = marker;
+module.exports = buildMarker;
